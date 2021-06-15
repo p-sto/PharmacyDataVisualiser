4241ad1915f5dcd914d4ee99d1b6e8309c47e415
stovorov <stoworow@gmail.com>
2021-06-15 15:38:33 +0200

    Correct errors

--- dataVisualizer/functions.py
+++ dataVisualizer/functions.py
@@ -1,24 +1,24 @@
 """Stores implementations of different functions"""
-from typing import List, Dict, Tuple, Optional
+from typing import List, Dict, Tuple, Optional, cast
 
 from pandas import DataFrame
 import numpy as np
 
-from dataVisualizer.data_containers import results_matrix
-from dataVisualizer.input_data_parser.parsers import SingleFileParser, DoubleFileParser
+from dataVisualizer.data_containers.results_matrix import DataMatrix
+from dataVisualizer.input_data_parser.parsers import SingleFileParser, DoubleFileParser, ParserABC
 
 OUTPUT_EXCEL_FILE_NAME = 'ordered_data.xlsx'
 OUTPUT_EXCEL_FILE_NAME_TRANSPOSED = 'ordered_data_transposed.xlsx'
 
 
-def get_data_matrix(source_dir: str, data_fil_name: str, order_fil_name: Optional[str]) -> results_matrix:
+def get_data_matrix(source_dir: str, data_fil_name: str, order_fil_name: Optional[str]) -> ParserABC:
     """Get input data as DataFrame with ordered data"""
     if order_fil_name:
-        return SingleFileParser(source_dir, data_fil_name, order_fil_name).get_result_matrix()
-    return DoubleFileParser(source_dir, data_fil_name).get_result_matrix()
+        return cast(ParserABC, SingleFileParser(source_dir, data_fil_name, order_fil_name).get_result_matrix())
+    return cast(ParserABC, DoubleFileParser(source_dir, data_fil_name).get_result_matrix())
 
 
-def generate_output_excels(result_matrix: results_matrix, output_dir: str) -> None:
+def generate_output_excels(result_matrix: DataMatrix, output_dir: str) -> None:
     """Generate excel outputs"""
     result_matrix.data.to_excel('{}/{}'.format(output_dir, OUTPUT_EXCEL_FILE_NAME), index=False)
     result_matrix.transposed_data.to_excel('{}/{}'.format(output_dir, OUTPUT_EXCEL_FILE_NAME_TRANSPOSED))
@@ -31,7 +31,7 @@ def get_samples(data: DataFrame) -> Dict:
 
 def get_mean_and_std(data: List) -> Tuple[float, float]:
     """Calculate mean and std deviation for given data"""
-    return np.mean(data), np.std(data)
+    return float(np.mean(data)), float(np.std(data))
 
 
 def filter_extreme_values(data: List[float], filter_sigma: float = 3) -> List[float]:
